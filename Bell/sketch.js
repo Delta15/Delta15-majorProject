338cd1cb8424bad6a4aa3c7819e2fa53156d1c00
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-05-14 18:16:41 -0500

    save

--- Bell/sketch.js
+++ Bell/sketch.js
@@ -1,48 +1,17 @@
+let testBell;
+
+function preload(){
+  testBell = loadSound("Music&Sounds/mask_ Bell and String.mp3");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+  testBell.loop();
 }
 
 function draw() {
-  beat();
-}
-
-function beat(){
-  let bellONE, bellTWO, bellTHREE;
-  let state = 1;
-  bellONE = new Timer(0);
-  if (state === 1) {
-    background(0);
-    fill(255,0,0);
-    ellipse(random(width), random(height), 100, 100);
-    noLoop();
-    if (bellONE.isDone()) {
-      bellTWO = new Timer(500);
-      state = 2;
-      loop();
-    }
-  }
-  else if (state === 2) {
-    background(0);
-    fill(255,0,0);
-    ellipse(random(width), random(height), 100, 100);
-    noLoop();
-    if (bellTWO.isDone()) {
-      bellTHREE = new Timer(500);
-      state = 3;
-      loop();
-    }
-  }
-  else if (state === 3) {
-    background(0);
-    fill(255,0,0);
-    ellipse(random(width), random(height), 100, 100);
-    noLoop();
-    if (bellTHREE.isDone()) {
-      bellTHREE = new Timer(500);
-      // state = 4;
-    }
-  }
+  background(0);
 }
 
 function windowResized() {
