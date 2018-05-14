8f03579e4a87156374abb503e2f52b2a8d2325f3
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-05-13 21:01:49 -0500

    save

--- Bell/sketch.js
+++ Bell/sketch.js
@@ -1,34 +1,73 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
-
-// global variables
-let gear;
-
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-  background(255);
+  beat();
+}
 
-  image(gear, 0, 0);
+function beat(){
+  let bellONE, bellTWO, bellTHREE;
+  let state = 1;
+  bellONE = new Timer(0);
+  if (state === 1) {
+    background(0);
+    fill(255,0,0);
+    ellipse(random(width), random(height), 100, 100);
+    noLoop();
+    if (bellONE.isDone()) {
+      bellTWO = new Timer(500);
+      state = 2;
+      loop();
+    }
+  }
+  else if (state === 2) {
+    background(0);
+    fill(255,0,0);
+    ellipse(random(width), random(height), 100, 100);
+    noLoop();
+    if (bellTWO.isDone()) {
+      bellTHREE = new Timer(500);
+      state = 3;
+      loop();
+    }
+  }
+  else if (state === 3) {
+    background(0);
+    fill(255,0,0);
+    ellipse(random(width), random(height), 100, 100);
+    noLoop();
+    if (bellTHREE.isDone()) {
+      bellTHREE = new Timer(500);
+      // state = 4;
+    }
+  }
+}
 
-  stroke(0);
-  line(0, 0, 200, 200);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
-  fill(0, 255, 0, 100);
-  noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+class Timer {
+  constructor(waitTime) {
+    this.waitTime = waitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  reset(newWaitTime) {
+    this.waitTime = newWaitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  isDone() {
+    if (millis() >= this.finishTime) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
