ee92d1df9acd250d9db0b0ea6252cb8262e591b5
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-05-14 11:00:06 -0500

    save

--- String/sketch.js
+++ String/sketch.js
@@ -1,4 +1,4 @@
-
+let x = 50;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -15,7 +15,35 @@ function windowResized() {
 
 function string(){
   background(0);
+  x = x + random(50, windowWidth - 50);
+  if (x > width) {
+    x = 0;
+  }
   stroke(255,0,0);
-  strokeWeight(3);
-  line(windowWidth/2, windowHeight, windowWidth/2, 0);
+  strokeWeight(5);
+  line(x, 0, x, height);
+  noLoop();
+}
+
+class Timer {
+  constructor(waitTime) {
+    this.waitTime = waitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  reset(newWaitTime) {
+    this.waitTime = newWaitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  isDone() {
+    if (millis() >= this.finishTime) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
