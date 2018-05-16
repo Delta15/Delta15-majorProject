df0a87f5c9f010e36cfe37bbf6b87a45832f8b01
Delta15 <36054022+Delta15@users.noreply.github.com>
2018-05-16 10:39:09 -0500

    save

---
+++ window&rain/sketch.js
@@ -0,0 +1,62 @@
+//McRavenTuazon
+//Cs30
+//15-May-2018
+
+//Reference
+// https://genius.com/Lyn-inaizumi-beneath-the-mask-annotated
+//https://www.youtube.com/watch?v=h012heXDB7o
+
+//rain sound effect
+let rainSfx;
+
+function preload() {
+  rainSfx = loadSound("Music&Sounds/rain.wav");
+  //A looping sound of rain
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rainSfx.loop();
+  rainSfx.setVolume(0.3);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  windowM();
+}
+
+function windowM() {
+  background(0);
+  fill(255);
+  rectMode(CENTER, CENTER);
+  rect(width/2 - 350,height/2,500,800);
+  fill(0);
+  rect(width/2 - 350,height/2,485,785);
+}
+
+
+class Timer {
+  constructor(waitTime) {
+    this.waitTime = waitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  reset(newWaitTime) {
+    this.waitTime = newWaitTime;
+    this.startTime = millis();
+    this.finishTime = this.startTime + this.waitTime;
+    this.timerIsDone = false;
+  }
+  isDone() {
+    if (millis() >= this.finishTime) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
